5cf483f038ae11959779cba72cfefb1d6e162d1e
Elias <eliaslleragr@gmail.com>
2022-03-23 13:34:15 +0100

    SDI-2122-406-7.5-Vistas y motores de plantillas.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,14 +1,28 @@
 module.exports = function (app) {
 
     app.get("/songs", function (req, res) {
-        let response = "";
+        let songs = [{
+            "title":"Blank space",
+            "price": "1.2"
+        }, {
+            "title":"See you again",
+            "price": "1.3"
+        }, {
 
-        if(req.query.title != null && typeof(req.query.title) != "undefined")
-            response = 'Titulo: ' + req.query.title + '<br>';
-        if(req.query.author != null && typeof(req.query.author) != "undefined")
-            response += 'Autor: ' + req.query.author;
+            "title":"Uptown Funk",
+            "price": "1.1"
+        }];
 
-        res.send(response);
+        let response = {
+            seller: "Tienda de canciones",
+            songs: songs
+        };
+
+        res.render("shop.twig", response);
+    });
+
+    app.get('/songs/add', function(req, res){
+        res.render("add.twig");
     });
 
 
@@ -23,13 +37,7 @@ module.exports = function (app) {
         res.send(response);
     });
 
-    app.post('/songs/add', function(req, res){
-       let response = "Cancion agregada: " + req.body.title + "<br>"
-       + " genero: " + req.body.kind + "<br>"
-       + " precio: " + req.body.price;
 
-       res.send(response);
-    });
 
     app.get('/add', function(req, res) {
         let response = parseInt(req.query.num1) + parseInt(req.query.num2);;
