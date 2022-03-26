c496c919148a845cf1d3cb5152dae17a6bfc7588
Elias <eliaslleragr@gmail.com>
2022-03-26 14:19:50 +0100

    SDI–2122-406-7.101-Ejercicio complementario 1

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+
 require("./routes/songs.js")(app);
+require("./routes/authors.js")(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ musicstoreapp/routes/authors.js
@@ -0,0 +1,60 @@
+module.exports = function (app) {
+
+    app.get("/authors", function (req, res) {
+        let authors = [{
+            "name":"Autor1",
+            "group": "Grupo1",
+            "rol": "Guitarrista"
+        }, {
+            "name":"Autor2",
+            "group": "Grupo2",
+            "rol": "Guitarrista"
+        }, {
+            "name":"Autor3",
+            "group": "Grupo2",
+            "rol": "Bajista"
+        }];
+
+        let response = {
+            authors: authors
+        };
+
+        res.render("authors/authors.twig", response);
+    });
+
+    app.post('/authors/add', function(req, res){
+        let response = 'Autor:';
+
+        response += ' Nombre: '
+        if(req.body.name != null && typeof req.body.name != undefined && req.body.name.trim().length !=0){
+            response += req.body.name;
+        } else {
+            response += 'No enviado en la petición';
+        }
+
+        response += ' Grupo: '
+        if(req.body.group != null && typeof req.body.group != undefined && req.body.group.trim().length !=0){
+            response += req.body.group;
+        } else {
+            response += 'No enviado en la petición';
+        }
+
+        response += ' Rol: '
+        if(req.body.rol != null && typeof req.body.rol != undefined && req.body.rol.trim().length !=0){
+            response += req.body.rol;
+        } else {
+            response += 'No enviado en la petición';
+        }
+
+        res.send(response);
+    });
+
+    app.get('/authors/add', function(req, res){
+        res.render("authors/add.twig");
+    });
+
+    app.get("/authors/*" , function (req, res) {
+        res.redirect("/authors");
+    });
+
+};
